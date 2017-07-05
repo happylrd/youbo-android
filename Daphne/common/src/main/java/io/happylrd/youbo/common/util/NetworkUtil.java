92bda98d63ca3fa78d259a7668f8ba028763b668
Graham Tang <airfree1452@gmail.com>
2017-07-05 15:48:10 +0800

    Create tools

---
+++ Daphne/common/src/main/java/io/happylrd/youbo/common/util/NetworkUtil.java
@@ -0,0 +1,15 @@
+package io.happylrd.youbo.common.util;
+
+/**
+ * <h1>modify class name here</h1>
+ * io.happylrd.youbo.common.util
+ * <p>
+ * add description here
+ *
+ * @author Crepusculo
+ * @version 1.0
+ * @since 2017/7/5
+ */
+
+public class NetworkUtil {
+}
